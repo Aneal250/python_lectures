1d8ea0cf3a0dee7ae4a64a09625b4df2beb98cc3
aneal250 <anaelennaemeka@gmail.com>
2025-08-10 19:05:19 +0100

    task: updated

---
+++ Task/day2.py
@@ -0,0 +1,19 @@
+
+Task 1 
+# b) Once you have written the class, write a program that creates three Employee objects to
+# hold the following data:
+# Name ID Number Department Job Title
+# Susan Meyers 47899 Accounting Vice President
+# Mark Jones 39119 Programmer
+# Joy Rogers 81774 Manufacturing Engineer
+# The program should store this data in the three objects and then display the data for each
+# employee on the screen. 
+
+
+Task 2
+# a) Add a method to the Employee class that allows you to change the department of an employee.
+# Demonstrate this method by changing the department of one of the employee objects and displaying the updated information.
+
+
+Task 3 
+Watch the Video, 
